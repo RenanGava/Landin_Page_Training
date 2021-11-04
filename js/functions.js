8bcc14cb1d6ea85bcb5927d5231d76770a439948
RenanGava <renandellecrodegava7040@gmail.com>
2021-11-04 19:46:10 -0300

    add google maps API 3

--- js/functions.js
+++ js/functions.js
@@ -2,12 +2,24 @@ window.onload = function(){
     let map;
 
     function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -20.6733669, lng: -41.0106183 },
-        zoom: 8,
-        
-    });
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: -20.6733669, lng: -41.0106183 },
+            zoom: 8,
+            scrollWheel: false
+            
+        });
+
+    
     }
 
+    // function animal(){
+    //     this.nome = 'cachorro'
+    //     this.peso = '24kg'	
+    // }
+    
+    
+    // var meuAnimal = new animal();
+
+    // console.log(meuAnimal);
     initMap();
 }
